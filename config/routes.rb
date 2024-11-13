e0f18cf9530423b4e8487e4e01d1f87cf226b3cf
Leonidas Zamora Gottwald <leonidas.zamora.gottwald@gmail.com>
2024-11-13 15:54:03 +0100

    finished the entire excercise 7/10

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :restaurants, only: %i[index new show create] do
-    resources :reviews, only: [:new, :index, :create]
+    resources :reviews, only: [ :index, :create]
   end
   resources :reviews, only: [:show, :destroy]
   # Defines the root path route ("/")

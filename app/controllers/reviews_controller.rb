b8bb7cb9ec341644b163faf891823ee251054e9d
Leonidas Zamora Gottwald <leonidas.zamora.gottwald@gmail.com>
2024-11-13 15:25:03 +0100

    finished now combining the thing so i think that is the refactoring 8/10

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,37 @@
+class ReviewsController < ApplicationController
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create(
+  name: 'Belcanto',
+  address: 'Avenue du perou 33',
+  phone_number: '+39 458834885',
+  category: 'french'
+)
+
+Restaurant.create(
+  name: 'Losteria',
+  address: 'Time square, New York',
+  phone_number: '+97 45919402228',
+  category: 'chinese'
+)
+Restaurant.create(
+  name: 'Mundgerecht',
+  address: 'Theatherplatz, Ingolstadt 24',
+  phone_number: '+49 772801239',
+  category: 'italian'
+)
+Restaurant.create(
+  name: 'Mamma mia',
+  address: 'Avenue de la florentina 273',
+  phone_number: '+28 2349959',
+  category: 'italian'
+)
+Restaurant.create(
+  name: 'Bong chou',
+  address: 'Shibuya crossing Tokyo',
+  phone_number: '+81 320 492340',
+  category: 'japanese'
+)
